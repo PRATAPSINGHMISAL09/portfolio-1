7798c0c308b48e47dc3b3c224fc7d457cfb0eaae
Pratap <pratapmisalngu@gmail.com>
2024-11-08 20:46:44 +0530

    updated background main page

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ const App = () => {
   return (
     <div className = "overflow-x-hidden text-stone-300 antialiased">
         <div className="fixed inset-0 -z-10">
-        <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
-        <div className="container mx-auto px-8"></div>
-        <Navbar></Navbar>
+        <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>      <div className="container mx-auto px-8"></div>
+        <Navbar/>
         </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     <nav className="flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
             <a href="/" aria-label='Home'>
-            <img src={logo} className='mx-2 backdrop-blur-2xl' width={50} height={50}  /></a>
+            <img src={logo} className="mx-2" width={50} height={50} /></a>
         </div>
 
     </nav>
